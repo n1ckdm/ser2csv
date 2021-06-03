cdfcb39f855c304b1bd95d3203e7ced43d6805b6
Nicholas Martin <nicholas.martin@tharsus.co.uk>
2021-06-03 01:10:13 +0100

    run pytest on install and add readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# ser2csv
+
+Reads data from a serial port and sends to a csv file
+
+## Install
+
+```bash
+install.ps1
+```
+
+## Example usage
+
+```bash
+python ser2csv.py --help
+python .\ser2csv.py --time-secs 5 delta-t 0.1 COM1
+```
--- install.ps1
+++ install.ps1
@@ -1,4 +1,5 @@
 python -m venv .env --prompt ser2csv
 ./.env/Scripts/Activate.ps1
 pip install -r .\requirements.txt
-python .\ser2csv.py COM1 --time-secs 5 delta-t 0.1
+pytest .\ser2csv.py
+# python .\ser2csv.py --time-secs 5 delta-t 0.1 COM1
